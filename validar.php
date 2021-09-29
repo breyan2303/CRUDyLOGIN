4f89adc3e8e065eb74f2d107c8a3758bf5149237
Breyan <breyanjuniort@gmail.com>
2021-09-29 01:56:46 -0500

    Se agrega el Proyecto

---
+++ validar.php
@@ -0,0 +1,33 @@
+<?php
+$usuario=$_POST['usuario'];
+
+$contraseña=$_POST['contraseña'];
+
+session_start();
+
+$_SESSION['usuario']=$usuario;
+
+$conexion=mysqli_connect("localhost","root","","boss");
+
+$consulta="SELECT*FROM login where usuario='$usuario' and contraseña='$contraseña'";
+
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:Lista.html");
+
+}else{
+    ?>
+    <?php
+    include("index.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AL INGRESAR</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+
+mysqli_close($conexion);
